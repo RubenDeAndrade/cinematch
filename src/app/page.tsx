a75f972c2fdbab09c771d7ca18cdaf2ba375aacd
Matthieu-EY <matthieu.sirvan@fabernovel.ey.com>
2025-07-10 14:38:05 +0200

    feat: page liste pokemon, affichage en grid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,7 @@ export default function HomePage() {
   */
 
   const pokemonsQuery = trpc.poke.list.useInfiniteQuery({
-    limit: 50,
+    limit: 49,
   }, {
     getNextPageParam(lastPage) {
       const nextCursor = parseInt(getTokenFromUrl(lastPage.next, 6), 10);
@@ -40,39 +40,40 @@ export default function HomePage() {
           Pokédex
         </h1>
 
-        <div className="mt-4 flex flex-col gap-4">
+        <div className="mt-4 grid justify-center items-center content-center justify-items-center grid-flow-row grid-cols-[repeat(auto-fit,150px)] auto-rows-auto gap-4">
           {pokemons.map(({ data: pokemon }, index) => (
                 <div
                   key={index + 1}
-                  className="rounded-md border border-gray-600 bg-gray-700 p-4"
+                  className="flex flex-col justify-center items-center rounded-md border border-gray-600 bg-gray-700 p-4 w-[150px] h-[200px]"
                 >
-                  <h2 className="text-2xl font-bold">{pokemon?.name}</h2>
+                  <h2 className="text-2xl font-bold capitalize text-nowrap">{pokemon?.name}</h2>
                   {pokemon?.sprites.front_default && (
                     <img src={pokemon?.sprites.front_default ?? ''} className="max-w-[80px]" />
                   )}
                   <Link
                     href={`/pokemon/${pokemon?.id}`}
-                    className="mt-2 inline-block rounded-md bg-indigo-500 px-4 py-2 text-white hover:bg-indigo-600"
+                    className="mt-2 inline-block text-center rounded-md bg-indigo-500 px-4 py-2 text-white hover:bg-indigo-600"
                   >
                     View
                   </Link>
                 </div>
           ))}
-          <div className="flex justify-center">
-            <button
-              className="rounded-md bg-indigo-500 px-4 py-2 text-white hover:bg-indigo-600"
-              onClick={() => void pokemonsQuery.fetchNextPage()}
-              disabled={
-                !pokemonsQuery.hasNextPage || pokemonsQuery.isFetchingNextPage
-              }
-            >
-              {pokemonsQuery.isFetchingNextPage
-                ? 'Loading more...'
-                : pokemonsQuery.hasNextPage
-                  ? 'Load more'
-                  : 'No more pokémon'}
-            </button>
-          </div>
+        </div>
+
+        <div className="w-full flex justify-center">
+          <button
+            className="rounded-md bg-indigo-500 px-4 py-2 mt-6 text-white hover:bg-indigo-600"
+            onClick={() => void pokemonsQuery.fetchNextPage()}
+            disabled={
+              !pokemonsQuery.hasNextPage || pokemonsQuery.isFetchingNextPage
+            }
+          >
+            {pokemonsQuery.isFetchingNextPage
+              ? 'Loading more...'
+              : pokemonsQuery.hasNextPage
+                ? 'Load more'
+                : 'No more pokémon'}
+          </button>
         </div>
       </div>
     </DefaultLayout>
